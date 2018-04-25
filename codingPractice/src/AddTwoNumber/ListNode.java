8d159e90232c10495f41e6d28f5858ecd060e67c
shindn <shindn89@gmail.com>
2018-04-24 22:58:26 -0400

    Initial checkin

---
+++ codingPractice/src/AddTwoNumber/ListNode.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package AddTwoNumber;
+
+/**
+ * @author DN
+ *
+ */
+public class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x) { val = x; }
+}
---
+++ codingPractice/src/pourWater/pourWater.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package pourWater;
+
+/**
+ * @author DN
+ *
+ */
+public class pourWater {
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println((20)/10);
+		int first= 10;
+		int second = 10;
+		int val;
+		val = (first+second)%10==0 ? (val = 1): (val=2);
+		System.out.println(val);
+	}
+
+}
